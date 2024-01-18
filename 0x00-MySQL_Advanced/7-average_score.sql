b2c218d82b485eb8f48f40a6d2bb121a8ef41f7a
TonnyMpho <tonnympho1@gmail.com>
2024-01-18 15:02:11 -0800

    MySQL advanced

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -8,7 +8,7 @@ CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
 	DECLARE avg_score DECIMAL(10, 2);
 
-	SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+	SELECT AVG(score) INTO avg_score FROM corrections WHERE ucorrections.ser_id = user_id;
 	UPDATE users SET average_score = avg_score WHERE id = user_id;
 END //
 
