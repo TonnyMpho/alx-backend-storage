c1fb14bc4d7b67e0fe63c1d77db32aa9a78240df
TonnyMpho <tonnympho1@gmail.com>
2024-01-22 13:03:34 -0800

    NoSQL

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" List all documents in Python """
+
+def list_all(mongo_collection):
+    """ Python function that lists all documents in a collection """
+    documents = mongo_collection.find()
+
+    if documents:
+        return [document for document in documents]
+    return []
