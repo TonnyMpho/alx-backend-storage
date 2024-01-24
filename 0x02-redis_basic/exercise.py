fc23e412d5d2b2490651de97522ffa301e4429cf
TonnyMpho <tonnympho1@gmail.com>
2024-01-24 14:26:51 -0800

    Redis basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Writing strings to Redis """
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -23,3 +23,43 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float, None]:
+        """
+        method that take a key string argument and an optional Callable
+        argumen named fn. The callable converts the data back to
+        the desired format
+        """
+        data = self._redis.get(key)
+        if data is not None and fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        automatically parametrize Cache.get with the correct conversion
+        function
+        """
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """
+        automatically parametrize Cache.get with the correct conversion
+        function
+        """
+        return self.get(key, fn=int)
+
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
