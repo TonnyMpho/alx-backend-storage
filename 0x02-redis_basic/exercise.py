061ec4ac197239427a244576490b77fee473946e
TonnyMpho <tonnympho1@gmail.com>
2024-01-24 13:05:52 -0800

    Redis basic

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Writing strings to Redis """
+import uuid
+import redis
+from typing import Union
+
+
+class Cache:
+    """ Cache class """
+    def __init__(self):
+        """
+        method, store an instance of the Redis client as a private
+        variable named _redis and flush the instance
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        method that takes a data argument and returns a string.
+        The method generate a random key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
