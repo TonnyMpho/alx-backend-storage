7d0c39500eacd63597cb3da79c05cc7faa5ff85e
TonnyMpho <tonnympho1@gmail.com>
2024-01-24 21:36:04 -0800

    Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -39,7 +39,7 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
-def replay(method: Callable):
+def replay(method: Callable) -> None:
     """  function to display the history of calls of a particular function """
     cache = redis.Redis()
     input_key = "{}:inputs".format(method.__qualname__)
